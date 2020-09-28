768cda604dc7d1da3404bacb98745348ffc3df59
KONDWANI_NGULUBE <kondwanigideon.ngulube@gmail.com>
2020-09-28 08:01:06 +0200

    second commit

--- test.java
+++ test.java
@@ -1,6 +1,35 @@
+//import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world");   
+        System.out.println("Hello world"); 
+        //Lines of code
+        System.out.println("first line of code");  
+        System.out.println("More lines of code");
+        /* input data
+        Scanner testVar = new Scanner(System.in);
+        System.out.println(testVar.nextLine());
+        */
+        
+        //data types
+        int myAge = 10* 2;
+        String myName = "Kondwani";
+        double temp = 30.5;
+        char group = 'z';
+        System.out.println(myAge);
+        System.out.println(myName);
+
+        //prefix
+        int x = 24;
+        System.out.println(x);
+        System.out.println(++x);
+
+        //post fix
+
+        int y = 17;
+        System.out.println(y);
+        System.out.println(y++);
+
     }
 }
 
